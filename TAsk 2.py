3b25d39829fb5c9f33e0f011e44bbb8863119fda
Gufran <mdgufrannasir@gmail.com>
2025-08-15 20:33:14 +0530

    final commit

---
+++ TAsk 2.py
@@ -0,0 +1,6 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+firstfive = list[0:5]
+print(f'list: {list}')
+print(f'Extracts the first five elements: {firstfive}')
+firstfive.reverse()
+print(f'Reverses these extracted elements:{firstfive}')
---
+++ Task 1.py
@@ -0,0 +1,6 @@
+dist = {'Rooman': 80, 'Faisal':70, 'Zaif':75,}
+name = input("Enter the student's name: ")
+if name in dist:
+    print(f"{name}'s marks: {dist[name]}")
+else:
+    print('Student not found')
